0c909654b6c54ea6b194395237669d442860f623
Patrick Conant <patrick@prcapps.com>
2018-01-06 23:27:21 -0500

    style updates

--- index.js
+++ index.js
@@ -16,16 +16,16 @@ app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/node_modules"));
 
 app.get("/about", function(request, response) {
-  response.render("about");
+	response.render("about");
 });
 
 app.get("/stats", function(request, response) {
 	var obj;
 	fs.readFile("public/data/keen_data_long.json", "utf8", function (err, data) {
-	  if (err){
-		throw err;
-	  } 
-	  obj = JSON.parse(data);
+		if (err){
+			throw err;
+		} 
+		obj = JSON.parse(data);
 		response.render("stats", { keenData: obj});
 	});
 });
@@ -34,63 +34,44 @@ app.get("/", function(request, response) {
 	var obj;
 	var obj2;
 	fs.readFile("public/data/historical_data.json", "utf8", function (err, data) {
-	  if (err) {
-		throw err;
-	  }
-	  obj = JSON.parse(data);
-
-	  fs.readFile("public/data/keen_data.json", "utf8", function (err, data) {
 		if (err) {
-		  throw err;
+			throw err;
 		}
-		obj2 = JSON.parse(data);
+		obj = JSON.parse(data);
 
-		response.render("index", { activeData: obj, keen_data: obj2});
+		fs.readFile("public/data/keen_data.json", "utf8", function (err, data) {
+			if (err) {
+				throw err;
+			}
+			obj2 = JSON.parse(data);
 
-	  });
-
-
-
-
+			response.render("index", { activeData: obj, keen_data: obj2});
+		});
 	});
-
-
-	// response.sendFile(__dirname + "/public/html/index.html");
 });
 
 app.get("/data", function(request, response) {
-  response.sendFile(__dirname + "/public/data/keen_data.json");
+	response.sendFile(__dirname + "/public/data/keen_data.json");
 });
 
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
 
-
 io.on("connection", function(socket){
-  console.log("a user connected");
+	console.log("a user connected");
+	io.emit("spaces-update", getKeenData.activeSpacesData);
 
-  // console.log( getKeenData.activeSpacesData);
-  io.emit("spaces-update", getKeenData.activeSpacesData);
-
-  socket.on("disconnect", function(){
-	  console.log("user disconnected");
+	socket.on("disconnect", function(){
+		console.log("user disconnected");
 	});
-
-  // socket.on("spaces-update", function(msg){
-  //	   console.log("message" + msg);
-
-  //	 io.emit("chat message", msg);
-  //   });
 });
-
-
 
 getKeenData.monitor_check_space_data(io);
 processKeenData.monitorProcessKeenData(io);
 getKeenData.check_space_data();
 
 http.listen(app.get("port"), function() {
-  console.log("Node app is running at localhost:" + app.get("port"));
+	console.log("Node app is running at localhost:" + app.get("port"));
 })
 
 
