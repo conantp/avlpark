59ef6b7a8cdc3ba421b6bc4aa84a9aae1c8966d1
Patrick Conant <patrick@prcapps.com>
2018-01-06 23:43:18 -0500

    style updates

--- public/js/app.js
+++ public/js/app.js
@@ -118,9 +118,10 @@ function renderKeenData(){
 	};
 
 	for(var deck in keen_data){
-		var keys = [];
 
 		if ({}.hasOwnProperty.call(keen_data, deck)) {
+			var keys = [];
+	
 			for(var k in keen_data[deck]) keys.push(k.substr(11, 5));
 
 			var tempData3 = {
